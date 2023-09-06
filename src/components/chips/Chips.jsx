24bc42e10a52a1c2111f9f739577f47c83733035
David Pallarés <serallapdivad@gmail.com>
2023-09-06 13:01:26 +0100

    Changed search bar root styles

--- src/components/chips/Chips.jsx
+++ src/components/chips/Chips.jsx
@@ -13,112 +13,43 @@ const Chips = () => {
     <>
       <Stack direction='row' spacing={1}>
         <Chip
-          sx={{
-            marginTop: '10px',
-            height: '36px',
-            width: '100px',
-            borderRadius: '4px',
-            fontWeight: 'bold',
-            borderColor: '#9d81b685',
-            backgroundColor: '#9d81b615',
-          }}
-          style={{ color: '#9d81b6' }}
+          sx={chipStyle}
           variant='outlined'
           label='Dog'
           onClick={handleClick}
         />
         <Chip
-          sx={{
-            marginTop: '10px',
-            height: '36px',
-            width: '100px',
-            borderRadius: '4px',
-            fontWeight: 'bold',
-            borderColor: '#9d81b685',
-            backgroundColor: '#9d81b615',
-          }}
-          style={{ color: '#9d81b6' }}
-          color='primary'
+          sx={chipStyle}
           variant='outlined'
           label='One Piece'
           onClick={handleClick}
         />
         <Chip
-          sx={{
-            marginTop: '10px',
-            height: '36px',
-            width: '100px',
-            borderRadius: '4px',
-            fontWeight: 'bold',
-            borderColor: '#9d81b685',
-            backgroundColor: '#9d81b615',
-          }}
-          style={{ color: '#9d81b6' }}
-          color='primary'
+          sx={chipStyle}
           variant='outlined'
           label='Rock'
           onClick={handleClick}
         />
         <Chip
-          sx={{
-            marginTop: '10px',
-            height: '36px',
-            width: '100px',
-            borderRadius: '4px',
-            fontWeight: 'bold',
-            borderColor: '#9d81b685',
-            backgroundColor: '#9d81b615',
-          }}
-          style={{ color: '#9d81b6' }}
-          color='primary'
+          sx={chipStyle}
           variant='outlined'
           label='Ice'
           onClick={handleClick}
         />
         <Chip
-          sx={{
-            marginTop: '10px',
-            height: '36px',
-            width: '100px',
-            borderRadius: '4px',
-            fontWeight: 'bold',
-            borderColor: '#9d81b685',
-            backgroundColor: '#9d81b615',
-          }}
-          style={{ color: '#9d81b6' }}
-          color='primary'
+          sx={chipStyle}
           variant='outlined'
           label='Keyboard'
           onClick={handleClick}
         />
         <Chip
-          sx={{
-            marginTop: '10px',
-            height: '36px',
-            width: '100px',
-            borderRadius: '4px',
-            fontWeight: 'bold',
-            borderColor: '#9d81b685',
-            backgroundColor: '#9d81b615',
-          }}
-          style={{ color: '#9d81b6' }}
-          color='primary'
+          sx={chipStyle}
           variant='outlined'
           label='React'
           onClick={handleClick}
         />
         <Chip
-          sx={{
-            marginTop: '10px',
-            height: '36px',
-            width: '100px',
-            borderRadius: '4px',
-            fontWeight: 'bold',
-            borderColor: '#9d81b685',
-            backgroundColor: '#9d81b615',
-          }}
-          style={{ color: '#9d81b6' }}
-          color='primary'
+          sx={chipStyle}
           variant='outlined'
           label='Hate'
           onClick={handleClick}
@@ -129,3 +60,14 @@ const Chips = () => {
 }
 
 export default Chips
+
+const chipStyle = {
+  marginTop: '10px',
+  height: '36px',
+  width: '100px',
+  borderRadius: '4px',
+  fontWeight: 'bold',
+  borderColor: '#9d81b685',
+  backgroundColor: '#9d81b615',
+  color: '#9d81b6',
+}
--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -2,13 +2,35 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { findPicsByUserInput } from '../../features/search/searchThunks'
 import { clearSavedPicsByInput } from '../../features/search/searchSlice'
-import { Divider, IconButton, InputAdornment, TextField } from '@mui/material'
+import {
+  Box,
+  Divider,
+  IconButton,
+  InputAdornment,
+  TextField,
+  styled,
+} from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search'
 import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined'
 
+const CssTextField = styled(TextField)({
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      border: '2px solid',
+      borderColor: '#9d81b685',
+    },
+    '&:hover fieldset': {
+      borderColor: '#9d81b655',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: '#9d81b6',
+    },
+  },
+})
+
 const SearchBar = ({ isMobile }) => {
   const dispatch = useDispatch()
-  const [currentInput, setCurrentInput] = useState(' ')
+  const [currentInput, setCurrentInput] = useState({ text: '' })
   const [userInput, setUserInput] = useState('')
 
   const searchedInput = useSelector((state) => state.browsedImages.search.input)
@@ -19,16 +41,16 @@ const SearchBar = ({ isMobile }) => {
     } else {
       setUserInput('e.g: Black cat')
     }
-  }, [searchedInput])
+  }, [searchedInput, userInput])
 
   const clearInput = () => {
     if (currentInput.text !== ' ') {
-      setCurrentInput({ text: ' ' })
+      setCurrentInput({ text: '' })
       dispatch(clearSavedPicsByInput())
     }
   }
 
-  const handleSearch = (event) => {
+  const handleSearch = () => {
     if (
       currentInput.text === '' ||
       currentInput.text === undefined ||
@@ -42,48 +64,61 @@ const SearchBar = ({ isMobile }) => {
 
   return (
     <>
-      <TextField
-        value={currentInput.text === '' ? null : currentInput.text}
-        placeholder={userInput}
-        onChange={(event) => {
-          setCurrentInput({ text: event.target.value })
-        }}
-        size={isMobile ? 'small' : 'regular'}
-        id='outlined-basic'
-        variant='outlined'
-        onKeyDown={(event) => {
-          event.keyCode === 13 &&
-            dispatch(findPicsByUserInput(currentInput.text))
-        }}
-        style={{ color: '#4966A6' }}
-        color='primary'
-        sx={{
-          width: '80%',
-          marginLeft: 5,
-          marginRight: 0,
-          marginTop: 1,
-          marginBottom: 1,
-        }}
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position='end'>
-              <IconButton type='button' sx={{ p: '10px' }} onClick={clearInput}>
-                <CancelOutlinedIcon />
-              </IconButton>
-              <Divider sx={{ height: 28, m: 1 }} orientation='vertical' />
-              <IconButton
-                type='button'
-                sx={{ p: '10px' }}
-                onClick={handleSearch}
-              >
-                <SearchIcon />
-              </IconButton>
-            </InputAdornment>
-          ),
-        }}
-      />
+      <Box component='form' noValidate autoComplete='off'>
+        <CssTextField
+          autoFocus
+          placeholder={userInput}
+          value={currentInput.text}
+          onChange={(event) => {
+            setCurrentInput({ text: event.target.value })
+          }}
+          size={isMobile ? 'small' : 'regular'}
+          id='outlined-basic'
+          variant='outlined'
+          onKeyDown={(event) => {
+            if (event.keyCode === 13) {
+              event.preventDefault()
+              dispatch(findPicsByUserInput(currentInput.text))
+              console.log('im here')
+            }
+          }}
+          style={{ color: '#4966A6' }}
+          color='primary'
+          sx={inputStyle}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position='end'>
+                <IconButton
+                  type='button'
+                  sx={{ p: '10px' }}
+                  onClick={clearInput}
+                >
+                  <CancelOutlinedIcon />
+                </IconButton>
+                <Divider sx={{ height: 28, m: 1 }} orientation='vertical' />
+                <IconButton
+                  type='button'
+                  sx={{ p: '10px' }}
+                  onClick={handleSearch}
+                >
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
     </>
   )
 }
 
 export default SearchBar
+
+const inputStyle = {
+  width: '80%',
+  marginLeft: 5,
+  marginRight: 0,
+  marginTop: 1,
+  marginBottom: 1,
+  fontWeight: 'bold',
+}
